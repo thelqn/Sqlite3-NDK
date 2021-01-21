a6741a4859ea1d84406290c78ee98598b020ec9d
thelqn <thelqn@gmail.com>
2021-01-21 21:43:29 +0000

    Add sqlite wrapper for  java
    With this files you can access to the sqlite library and use it .

---
+++ app/src/main/java/com/thelqn/sqlite3/BuildVars.java
@@ -0,0 +1,16 @@
+/*
+ * This is the source code of Telegram for Android v. 7.x.x.
+ * It is licensed under GNU GPL v. 2 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright Nikolai Kudashov, 2013-2020.
+ */
+
+package com.thelqn.sqlite3;
+
+public class BuildVars {
+
+    public static boolean DEBUG_VERSION = true;
+    public static boolean LOGS_ENABLED = true;
+
+}
